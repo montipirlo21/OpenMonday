70a4ec6cb7ed16b9154cfff6aeb822b611275387
Davide Monticelli <davide.monticelli@hotmail.it>
2024-12-01 15:58:53 +0100

    Create 4 simple tests for GetBoardItemsByCursor

--- tests/OpenMonday.Tests/MondayDriver/MondayDriverServiceTests.cs
+++ tests/OpenMonday.Tests/MondayDriver/MondayDriverServiceTests.cs
@@ -2,6 +2,7 @@ using Moq;
 using OpenMonday.Core.MondayDriver.Services;
 using OpenMonday.Core.strawberryShake;
 using OpenMonday.Tests.MondayDriver.TestBuildes;
+using StrawberryShake;
 
 namespace OpenMonday.Tests.MondayDriver
 {
@@ -12,6 +13,7 @@ namespace OpenMonday.Tests.MondayDriver
         private readonly Mock<IMondayClient> _mondayClientMock;
         private readonly Mock<MondayDriverService> _mondayDriverService;
         private readonly Mock<MondayDriverBoardStructureConverterService> _mondayBoardStructureConverterService;
+        private readonly Mock<MondayDriverBoardItemsConverterService> _mondayDriverBoardItemsConverterService;
 
         public MondayDriverServiceTests()
         {
@@ -19,9 +21,10 @@ namespace OpenMonday.Tests.MondayDriver
             _mondayClientMock = new Mock<IMondayClient>();
 
             _mondayBoardStructureConverterService = new Mock<MondayDriverBoardStructureConverterService>();
+            _mondayDriverBoardItemsConverterService = new Mock<MondayDriverBoardItemsConverterService>();
 
             // Moq MondayDriverService
-            _mondayDriverService = new Mock<MondayDriverService>(_mondayClientMock.Object, _mondayBoardStructureConverterService.Object)
+            _mondayDriverService = new Mock<MondayDriverService>(_mondayClientMock.Object, _mondayBoardStructureConverterService.Object, _mondayDriverBoardItemsConverterService.Object)
             {
                 // Base call where it is not mocked
                 CallBase = true
@@ -92,6 +95,115 @@ namespace OpenMonday.Tests.MondayDriver
             Assert.Equal(simulatedBoard.Data.Boards.ElementAt(0).Columns.ElementAt(0).Id, result.Data.BoardColumns.ElementAt(0).Id);
             Assert.Equal(simulatedBoard.Data.Boards.ElementAt(0).Columns.ElementAt(0).Title, result.Data.BoardColumns.ElementAt(0).Title);
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetBoardItemsByCursor_SimpleBoardItemsByCursorResult_Empty()
+        {
+            // ARRANGE
+            var simulatedBoard = TestDataBuilders_MondayDriverBoardItems.GenerateSimpleBoardItemsByCursorResult_Empty();
+            string boardId = "xxxxx";
+
+            var mockIGetBoardsByIdQuery = new Mock<IGetBoardItemsByCursorQuery>();
+            // Setup del monday client
+            mockIGetBoardsByIdQuery
+                .Setup(x => x.ExecuteAsync(It.IsAny<IReadOnlyList<string>>(), CancellationToken.None))
+                .ReturnsAsync(simulatedBoard);
+
+            _mondayClientMock
+                .Setup(x => x.GetBoardItemsByCursor)
+                .Returns(mockIGetBoardsByIdQuery.Object);
+
+            // ACT       
+            var result = await _mondayDriverService.Object.GetBoardItemsByCursor(boardId);
+
+            // ASSERT
+            Assert.True(result.IsFailure);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public async Task GetBoardItemsByCursor_SimpleBoardItemsByCursorResult_Fail_Two()
+        {
+            // ARRANGE
+            var simulatedBoard = TestDataBuilders_MondayDriverBoardItems.GenerateSimpleBoardItemsByCursorResult_Two();
+            string boardId = "xxxxx";
+
+            var mockIGetBoardsByIdQuery = new Mock<IGetBoardItemsByCursorQuery>();
+            // Setup del monday client
+            mockIGetBoardsByIdQuery
+                .Setup(x => x.ExecuteAsync(It.IsAny<IReadOnlyList<string>>(), CancellationToken.None))
+                .ReturnsAsync(simulatedBoard);
+
+            _mondayClientMock
+                .Setup(x => x.GetBoardItemsByCursor)
+                .Returns(mockIGetBoardsByIdQuery.Object);
+
+            // ACT       
+            var result = await _mondayDriverService.Object.GetBoardItemsByCursor(boardId);
+
+            // ASSERT
+            Assert.True(result.IsFailure);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public async Task GetBoardItemsByCursor_SimpleBoardItemsByCursorResult_Success()
+        {
+            // ARRANGE
+            var simulatedBoard = TestDataBuilders_MondayDriverBoardItems.GenerateSimpleBoardItemsByCursorResult_Success();
+            string boardId = "xxxxx";
+
+            var mockIGetBoardsByIdQuery = new Mock<IGetBoardItemsByCursorQuery>();
+            // Setup del monday client
+            mockIGetBoardsByIdQuery
+                .Setup(x => x.ExecuteAsync(It.IsAny<IReadOnlyList<string>>(), CancellationToken.None))
+                .ReturnsAsync(simulatedBoard);
+
+            _mondayClientMock
+                .Setup(x => x.GetBoardItemsByCursor)
+                .Returns(mockIGetBoardsByIdQuery.Object);
+
+            // ACT       
+            var result = await _mondayDriverService.Object.GetBoardItemsByCursor(boardId);
+
+            // ASSERT
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+        }
+
+        [Fact]
+        public async Task GetBoardItemsByCursor_SimpleBoardItemsByCursorResultWithNextPage_Success()
+        {
+            // ARRANGE
+            var simulatedBoard = TestDataBuilders_MondayDriverBoardItems.GenerateSimpleBoardItemsByCursorResult_WithCursor_Success();
+            string boardId = "xxxxx";
+
+            // Setup GetBoardItemsByCursor
+            var mockIGetBoardsByIdQuery = new Mock<IGetBoardItemsByCursorQuery>();
+            mockIGetBoardsByIdQuery
+                .Setup(x => x.ExecuteAsync(It.IsAny<IReadOnlyList<string>>(), CancellationToken.None))
+                .ReturnsAsync(simulatedBoard);
+            _mondayClientMock
+                .Setup(x => x.GetBoardItemsByCursor)
+                .Returns(mockIGetBoardsByIdQuery.Object);
+
+            // Setup GetBoardItemsByCursor_NextItem
+            OperationResult<IGetBoardItemsByCursor_NextPageResult> nextPage = TestDataBuilders_MondayDriverBoardItems.GenerateSimpleBoardItemsByCursor_NextPageResultResult_Success();
+            var mockIGetBoardItemsByCursor_NextItemsPageQuery = new Mock<IGetBoardItemsByCursor_NextPageQuery>();
+            mockIGetBoardItemsByCursor_NextItemsPageQuery
+                .Setup(x => x.ExecuteAsync(It.IsAny<string>(), CancellationToken.None))
+                .ReturnsAsync(nextPage);
+            _mondayClientMock
+                .Setup(x => x.GetBoardItemsByCursor_NextPage)
+                .Returns(mockIGetBoardItemsByCursor_NextItemsPageQuery.Object);
+
+            // ACT       
+            var result = await _mondayDriverService.Object.GetBoardItemsByCursor(boardId);
+
+            // ASSERT
+            Assert.True(result.IsSuccess);
+            Assert.Equal(6, result.Data.Count());
         }
     }
 }
---
+++ tests/OpenMonday.Tests/TestBuildes/TestDataBuilders_MondayDriverBoardItems.cs
@@ -0,0 +1,148 @@
+using System.Text.Json;
+using OpenMonday.Core.strawberryShake;
+using StrawberryShake;
+
+
+namespace OpenMonday.Tests.MondayDriver.TestBuildes;
+
+
+public class TestDataBuilders_MondayDriverBoardItems
+{
+    public static OperationResult<IGetBoardItemsByCursorResult> GenerateSimpleBoardItemsByCursorResult_Empty()
+    {
+        List<GetBoardItemsByCursor_Boards_Board> b = new List<GetBoardItemsByCursor_Boards_Board> { };
+
+        IGetBoardItemsByCursorResult data = new GetBoardItemsByCursorResult(b);
+        var operationResult = new OperationResult<IGetBoardItemsByCursorResult>(data, null, null, null, null, null);
+        return operationResult;
+    }
+
+    public static OperationResult<IGetBoardItemsByCursorResult> GenerateSimpleBoardItemsByCursorResult_Two()
+    {
+        List<GetBoardItemsByCursor_Boards_Board> b = new List<GetBoardItemsByCursor_Boards_Board>{
+
+            new GetBoardItemsByCursor_Boards_Board(null),
+            new GetBoardItemsByCursor_Boards_Board(null)
+        };
+
+        IGetBoardItemsByCursorResult data = new GetBoardItemsByCursorResult(b);
+        var operationResult = new OperationResult<IGetBoardItemsByCursorResult>(data, null, null, null, null, null);
+        return operationResult;
+    }
+
+    public static OperationResult<IGetBoardItemsByCursorResult> GenerateSimpleBoardItemsByCursorResult_Success()
+    {
+        List<IGetBoardItemsByCursor_Boards_Items_page_Items> tasks = GenerateIGetBoardItemsByCursor_Boards_Items_page_Items();
+
+        List<GetBoardItemsByCursor_Boards_Board> b = new List<GetBoardItemsByCursor_Boards_Board> {
+
+            new GetBoardItemsByCursor_Boards_Board(
+                new GetBoardItemsByCursor_Boards_Items_page_ItemsResponse(null, tasks)
+            )
+         };
+
+        IGetBoardItemsByCursorResult data = new GetBoardItemsByCursorResult(b);
+        var operationResult = new OperationResult<IGetBoardItemsByCursorResult>(data, null, null, null, null, null);
+        return operationResult;
+    }
+
+    public static List<IGetBoardItemsByCursor_Boards_Items_page_Items> GenerateIGetBoardItemsByCursor_Boards_Items_page_Items()
+    {
+        return new List<IGetBoardItemsByCursor_Boards_Items_page_Items>(){
+
+            // ITEM 1
+            new GetBoardItemsByCursor_Boards_Items_page_Items_Item
+            ("1626989663", "Task 1",
+            new List<IGetBoardItemsByCursor_Boards_Items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_Boards_Items_page_Items_Column_values_PeopleValue("project_owner",
+                 JsonDocument.Parse("{\"changed_at\":\"2023-08-23T12:42:09.066Z\",\"personsAndTeams\":[{\"id\":66019177,\"kind\":\"person\"}]}").RootElement,
+
+                ColumnType.People, "cayayam389@janfab.com","PeopleValue")
+
+            }),
+
+            // ITEM 2
+            new GetBoardItemsByCursor_Boards_Items_page_Items_Item("1626989661", "Task 2",
+             new List<IGetBoardItemsByCursor_Boards_Items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_Boards_Items_page_Items_Column_values_PeopleValue("project_owner",
+                null, ColumnType.People, "","PeopleValue")
+
+            }),
+
+            // ITEM 3       
+            new GetBoardItemsByCursor_Boards_Items_page_Items_Item("1626989662", "Task 3",  new List<IGetBoardItemsByCursor_Boards_Items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_Boards_Items_page_Items_Column_values_PeopleValue("project_owner",
+                null, ColumnType.People, "","PeopleValue")
+
+            })
+        };
+    }
+
+    public static OperationResult<IGetBoardItemsByCursorResult> GenerateSimpleBoardItemsByCursorResult_WithCursor_Success()
+    {
+        List<IGetBoardItemsByCursor_Boards_Items_page_Items> tasks = GenerateIGetBoardItemsByCursor_Boards_Items_page_Items();
+
+        List<GetBoardItemsByCursor_Boards_Board> b = new List<GetBoardItemsByCursor_Boards_Board> {
+
+            new GetBoardItemsByCursor_Boards_Board(
+                new GetBoardItemsByCursor_Boards_Items_page_ItemsResponse("cursor", tasks)
+            )
+         };
+
+        IGetBoardItemsByCursorResult data = new GetBoardItemsByCursorResult(b);
+        var operationResult = new OperationResult<IGetBoardItemsByCursorResult>(data, null, null, null, null, null);
+        return operationResult;
+    }
+
+    public static OperationResult<IGetBoardItemsByCursor_NextPageResult> GenerateSimpleBoardItemsByCursor_NextPageResultResult_Success()
+    {
+        List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items> tasks = GenerateIGetBoardItemsByCursor_Boards__NextPageResult_Items_page_Items();
+
+        GetBoardItemsByCursor_NextPage_Next_items_page_ItemsResponse b = new GetBoardItemsByCursor_NextPage_Next_items_page_ItemsResponse(null, tasks);
+
+        IGetBoardItemsByCursor_NextPageResult data = new GetBoardItemsByCursor_NextPageResult(b);
+        var operationResult = new OperationResult<IGetBoardItemsByCursor_NextPageResult>(data, null, null, null, null, null);
+        return operationResult;
+    }
+
+    public static List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items> GenerateIGetBoardItemsByCursor_Boards__NextPageResult_Items_page_Items()
+    {
+        return new List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items>(){
+
+            // ITEM 1
+            new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Item
+            ("1626989663", "Task 1",
+            new List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values_PeopleValue("project_owner",
+                 JsonDocument.Parse("{\"changed_at\":\"2023-08-23T12:42:09.066Z\",\"personsAndTeams\":[{\"id\":66019177,\"kind\":\"person\"}]}").RootElement,
+
+                ColumnType.People, "cayayam389@janfab.com","PeopleValue")
+
+            }),
+
+            // ITEM 2
+            new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Item("1626989661", "Task 2",
+             new List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values_PeopleValue("project_owner",
+                null, ColumnType.People, "","PeopleValue")
+
+            }),
+
+            // ITEM 3       
+            new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Item("1626989662", "Task 3",  new List<IGetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values>(){
+
+                new GetBoardItemsByCursor_NextPage_Next_items_page_Items_Column_values_PeopleValue("project_owner",
+                null, ColumnType.People, "","PeopleValue")
+
+            })
+        };
+    }
+
+    
+
+}
