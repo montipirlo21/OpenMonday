cba44ebee307d6aa13632cddf9ed1c9660318821
Davide Monticelli <davide.monticelli@fortech.it>
2025-01-22 14:41:24 +0100

    feat(updated_at): add updated_at information to GetBoardsStructureById and GetActivityLog

--- src/OpenMonday.Core/MondayDriver/Models/Structure/MondayDriverBoardStructure.cs
+++ src/OpenMonday.Core/MondayDriver/Models/Structure/MondayDriverBoardStructure.cs
@@ -3,21 +3,23 @@ public class MondayDriverBoardStructure
     public string BoardId { get; set; }
     public string BoardName { get; set; }
     public int? ItemsCount { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public List<MondayDriverColumnSchema> BoardColumns { get; set; }
     public List<MondayDriverGroupInformation> Groups { get; set; }
 
-    protected MondayDriverBoardStructure(string boardId, string boardName, int? itemsCount, List<MondayDriverColumnSchema> boardColumns, List<MondayDriverGroupInformation> groups)
+    protected MondayDriverBoardStructure(string boardId, string boardName, int? itemsCount, DateTime? updatedAt, List<MondayDriverColumnSchema> boardColumns, List<MondayDriverGroupInformation> groups)
     {
         this.BoardId = boardId;
         this.BoardName = boardName;
+        this.UpdatedAt = updatedAt;
         this.ItemsCount = itemsCount;
         this.BoardColumns = boardColumns;
         this.Groups = groups;
     }
 
-    public static MondayDriverBoardStructure Create(string id, string name, int? itemsCount, List<MondayDriverColumnSchema> boardColumns, List<MondayDriverGroupInformation> groups)
+    public static MondayDriverBoardStructure Create(string id, string name, int? itemsCount, DateTime? updatedAt, List<MondayDriverColumnSchema> boardColumns, List<MondayDriverGroupInformation> groups)
     {
-        return new MondayDriverBoardStructure(id, name, itemsCount, boardColumns, groups);
+        return new MondayDriverBoardStructure(id, name, itemsCount, updatedAt, boardColumns, groups);
     }
 
     public string FindColumnIdByNameOrStringEmpty(List<string> names)
--- src/OpenMonday.Core/Utils/DateTimeHelper.cs
+++ src/OpenMonday.Core/Utils/DateTimeHelper.cs
@@ -1,7 +1,25 @@
-public static class DateTimeConverter{
+public static class DateTimeConverter
+{
 
-    public static string ConvertToISO8601DateTimeString(DateTime dateTime){
+    public static string ConvertToISO8601DateTimeString(DateTime dateTime)
+    {
         return dateTime.ToString("yyyy-MM-ddTHH:mm:ssZ");
     }
 
+    public static DateTime? ConvertFromStringToISO8601DateTime(string? dateTime)
+    {
+        if (string.IsNullOrEmpty(dateTime))
+        {
+            return null;
+        }
+
+        if (DateTime.TryParseExact(dateTime, "yyyy-MM-ddTHH:mm:ssK", null, System.Globalization.DateTimeStyles.RoundtripKind, out DateTime d))
+        {
+            return d;
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
