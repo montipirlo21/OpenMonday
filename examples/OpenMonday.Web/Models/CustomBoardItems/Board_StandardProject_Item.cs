126ddf6fd397b397137457306e17f9caf07e59f5
Davide Monticelli <davide.monticelli@fortech.it>
2025-02-07 15:38:17 +0100

    fix(Board_Column_String_Value); set text value for mirror

--- examples/OpenMonday.Web/Models/CustomBoardItems/Board_StandardProject_Item.cs
+++ examples/OpenMonday.Web/Models/CustomBoardItems/Board_StandardProject_Item.cs
@@ -6,19 +6,24 @@ public class Board_StandardProject_Item : Board_Item
     public Board_Column_Status Status { get; set; }
     [ColumnMapping(searchingNames: ["Timeline", "Pianificazione"])]
     public Board_Column_Timeline Timeline { get; set; }
+    [ColumnMapping(searchingNames: ["Priority"])]
+    public Board_Column_String_Value Priority { get; set; }
 
     public Board_StandardProject_Item() { }
 
-    public Board_StandardProject_Item(string id, string name, string groupid, Board_Column_People owner, Board_Column_Status status, Board_Column_Timeline timeline) : 
+    public Board_StandardProject_Item(string id, string name, string groupid, Board_Column_People owner, Board_Column_Status status, 
+    Board_Column_Timeline timeline, Board_Column_String_Value priority) : 
     base(id, name, groupid)
     {
         Owner = owner;
         Status = status;
         Timeline = timeline;
+        Priority = priority;
     }
 
-    public static Board_StandardProject_Item Create(string id, string name,string groupid, Board_Column_People owner, Board_Column_Status status, Board_Column_Timeline timeline)
+    public static Board_StandardProject_Item Create(string id, string name,string groupid, Board_Column_People owner, Board_Column_Status status,
+     Board_Column_Timeline timeline, Board_Column_String_Value priority)
     {
-        return new Board_StandardProject_Item(id, name,groupid, owner, status, timeline);
+        return new Board_StandardProject_Item(id, name, groupid, owner, status, timeline, priority);
     }
 }
